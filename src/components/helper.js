8148666c67028e8609d267c91020f700a096c030
Javad Malekshahkoohi <mrjavad@gmail.com>
2019-09-29 10:44:58 +0200

    [jsx]. adding a helper for constant vars and shared functions

---
+++ src/components/helper.js
@@ -0,0 +1,7 @@
+export const DATE_FORMAT = "dddd, mmmm dS, h:MM:ss TT";
+
+export const SORT_BY_TYPE = {
+  AIRPORT_ORIGIN: "ariport_origin",
+  AIRPORT_DESTINATION: "ariport_destination",
+  TIME: "time"
+};
