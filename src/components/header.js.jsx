c715b26864999c0e2c800a2980b406d8982115fe
Javad Malekshahkoohi <mrjavad@gmail.com>
2019-09-29 10:45:57 +0200

    [jsx] creating header component

--- src/components/header.js.jsx
+++ src/components/header.js.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { FlexboxDiv, TextSpan } from "./layouts.js";
+import { SORT_BY_TYPE } from "./helper";
 
 const Header = props => {
   return (
-    <div>
+    <FlexboxDiv flexDirection="column">
       <h1>List of Flights</h1>
-      <div>SortBy options</div>
-    </div>
+      <FlexboxDiv>
+        <TextSpan>Sort By:</TextSpan>
+        <TextSpan onClick={() => console.log(SORT_BY_TYPE.TIME)}>Time</TextSpan>
+        <TextSpan onClick={() => console.log(SORT_BY_TYPE.AIRPORT_ORIGIN)}>
+          Airport Origin
+        </TextSpan>
+        <TextSpan onClick={() => console.log(SORT_BY_TYPE.AIRPORT_DESTINATION)}>
+          Airport Destination
+        </TextSpan>
+      </FlexboxDiv>
+    </FlexboxDiv>
   );
 };
 
