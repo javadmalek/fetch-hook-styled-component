b28bc8e2e26be4bedf496c09cf7da2aabdd521b5
Javad Malekshahkoohi <mrjavad@gmail.com>
2019-09-29 10:42:48 +0200

    [hook] adding a fetch hook to get data from or load from mocking data

---
+++ src/components/hook_fetch_flights.js.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import camelcaseKeys from "camelcase-keys";
+import MockingData from "../resources/mocking_data.json";
+
+const HookFetchFlights = url => {
+  const [data, setData] = useState([]);
+
+  const parseData = data => camelcaseKeys(data, { deep: true });
+
+  useEffect(() => {
+    if (!!url) {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => setData(parseData(data)));
+    } else {
+      setData(parseData(MockingData));
+    }
+  }, [url]);
+
+  return data;
+};
+
+export default HookFetchFlights;
