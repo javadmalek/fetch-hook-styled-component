4df29c397f1b6637be69e10f2f02769e3b8ce122
Javad Malekshahkoohi <mrjavad@gmail.com>
2019-09-29 10:46:32 +0200

    [jsx] creating flights component

--- src/components/containers_flights.js.jsx
+++ src/components/containers_flights.js.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Header from "./header.js";
 import Flights from "./flights.js";
 
-const ContainerFlights = props => {
+const ContainerFlights = ({ fetchedFlights }) => {
+  const [flights, setFlights] = useState(fetchedFlights || []);
+
+  useEffect(() => {
+    setFlights(fetchedFlights);
+  }, [fetchedFlights]);
+
+  // do resort here
+
   return (
     <div>
       <Header />
-      <Flights />
+      <Flights flights={flights} />
     </div>
   );
 };
--- src/components/flights.js.jsx
+++ src/components/flights.js.jsx
@@ -1,6 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
+import { FlexboxDiv } from "./layouts.js";
+import dateFormat from "dateformat";
+import { DATE_FORMAT } from "./helper";
 
-const renderFlight = ({
+const flight = ({
   segmentId,
   type,
   origin,
@@ -8,18 +11,35 @@ const renderFlight = ({
   departure,
   detail
 }) => {
+  const mainInfo = (
+    <div>
+      <span>{`${origin.value} -> ${destination.value}`}</span>
+      <span>{detail.flightNumber}</span>
+    </div>
+  );
+
+  const boardingInfo = (
+    <div>
+      <span>Departure: {dateFormat(departure, DATE_FORMAT)}</span>
+      <span>Boarding: {dateFormat(detail.boarding, DATE_FORMAT)}</span>
+      <span>Gate: {detail.gate}</span>
+      <span>Seat: {detail.seat}</span>
+    </div>
+  );
   return (
-    <div key={segmentId} w30 paddingDefault>
-      {segmentId}
+    <div key={segmentId}>
+      {mainInfo}
+      {boardingInfo}
     </div>
   );
 };
 
 const Flights = ({ flights }) => {
+  console.log(flights);
   if (flights === undefined || flights.length === 0) return null;
 
-  const list = flights.map(item => renderFlight(item));
-  return <div>{list}</div>;
+  const list = flights.map(item => flight(item));
+  return <FlexboxDiv flexWrap="wrap">{list}</FlexboxDiv>;
 };
 
 export default Flights;
