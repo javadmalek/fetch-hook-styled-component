1d434fa43115b04bffbaf8978811fd44a028ff2a
Javad Malekshahkoohi <mrjavad@gmail.com>
2019-09-29 11:23:03 +0200

    [jsx] adding resort functionality

--- src/components/header.js.jsx
+++ src/components/header.js.jsx
@@ -2,17 +2,34 @@ import React from "react";
 import { FlexboxDiv, TextSpan } from "./layouts.js";
 import { SORT_BY_TYPE } from "./helper";
 
-const Header = props => {
+const Header = ({ sortByType, resortFlightsFn }) => {
   return (
     <FlexboxDiv flexDirection="column">
       <h1>List of Flights</h1>
       <FlexboxDiv>
-        <TextSpan>Sort By:</TextSpan>
-        <TextSpan onClick={() => console.log(SORT_BY_TYPE.TIME)}>Time</TextSpan>
-        <TextSpan onClick={() => console.log(SORT_BY_TYPE.AIRPORT_ORIGIN)}>
+        <TextSpan marginRDefault>Sort By:</TextSpan>
+        <TextSpan
+          marginRDefault
+          linkLike
+          isActive={sortByType === SORT_BY_TYPE.TIME}
+          onClick={() => resortFlightsFn(SORT_BY_TYPE.TIME)}
+        >
+          Time
+        </TextSpan>
+        <TextSpan
+          marginRDefault
+          linkLike
+          isActive={sortByType === SORT_BY_TYPE.AIRPORT_ORIGIN}
+          onClick={() => resortFlightsFn(SORT_BY_TYPE.AIRPORT_ORIGIN)}
+        >
           Airport Origin
         </TextSpan>
-        <TextSpan onClick={() => console.log(SORT_BY_TYPE.AIRPORT_DESTINATION)}>
+        <TextSpan
+          marginRDefault
+          linkLike
+          isActive={sortByType === SORT_BY_TYPE.AIRPORT_DESTINATION}
+          onClick={() => resortFlightsFn(SORT_BY_TYPE.AIRPORT_DESTINATION)}
+        >
           Airport Destination
         </TextSpan>
       </FlexboxDiv>
